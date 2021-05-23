dfbe3ae4c8cbf5b2a54f012195b484e056425c09
jaymehta-ce <jaymehta.ce@gmail.com>
2021-05-23 14:31:18 +0530

    Update config.js

--- util/config.js
+++ util/config.js
@@ -1,6 +1,6 @@
 exports.config = {
   headers: {
-    "User-Agent": "axios app",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
   },
 };
 exports.options = {
